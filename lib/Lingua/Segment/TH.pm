ca2618b20da093a346d3fe8b9f7738cac4fd1548
Nick Patch <patch@cpan.org>
2013-06-12 20:30:29 -0400

    new project

---
+++ lib/Lingua/Segment/TH.pm
@@ -0,0 +1,51 @@
+package Lingua::Segment::TH;
+
+use v5.8;
+use utf8;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+Lingua::Segment::TH - Thai word segmenter
+
+=head1 VERSION
+
+This document describes Lingua::Segment::TH v0.01.
+
+=head1 SYNOPSIS
+
+    use Lingua::Segment::TH;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 SEE ALSO
+
+=over
+
+=item * ...
+
+=back
+
+=head1 AUTHOR
+
+Nick Patch <patch@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+© 2013 Nick Patch
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
---
+++ t/00-load.t
@@ -0,0 +1,8 @@
+use utf8;
+use strict;
+use warnings;
+use Test::More tests => 1;
+
+BEGIN { use_ok 'Lingua::Segment::TH' }
+
+diag "Testing Lingua::Segment::TH $Lingua::Segment::TH::VERSION, Perl $], $^X";
